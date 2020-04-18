098eb37d59cf18c7d896e4302e27842c22b83d16
晓辰 <xiaochen@dtstack.com>
2020-04-18 17:38:18 +0800

    fix db2

--- rdbms/db2/src/main/java/com/dtstack/dtcenter/common/loader/rdbms/db2/Db2ConnFactory.java
+++ rdbms/db2/src/main/java/com/dtstack/dtcenter/common/loader/rdbms/db2/Db2ConnFactory.java
@@ -2,6 +2,12 @@ package com.dtstack.dtcenter.common.loader.rdbms.db2;
 
 import com.dtstack.dtcenter.common.enums.DataBaseType;
 import com.dtstack.dtcenter.common.loader.rdbms.common.ConnFactory;
+import com.dtstack.dtcenter.loader.dto.SourceDTO;
+import com.dtstack.dtcenter.loader.utils.DBUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.Statement;
 
 /**
  * @company: www.dtstack.com
@@ -9,8 +15,26 @@ import com.dtstack.dtcenter.common.loader.rdbms.common.ConnFactory;
  * @Date ：Created in 16:09 2020/1/7
  * @Description：Db2 工厂类
  */
+@Slf4j
 public class Db2ConnFactory extends ConnFactory {
     public Db2ConnFactory() {
         this.driverName = DataBaseType.DB2.getDriverClassName();
+    }
+
+
+    @Override
+    public Boolean testConn(SourceDTO source) {
+        boolean isConnected = false;
+        Connection conn = null;
+        Statement statement = null;
+        try {
+            conn = getConn(source);
+            isConnected = true;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            DBUtil.closeDBResources(null, statement, conn);
+        }
+        return isConnected;
     }
 }
--- rdbms/db2/src/test/java/com/dtstack/dtcenter/common/loader/rdbms/db2/Db2ClientTest.java
+++ rdbms/db2/src/test/java/com/dtstack/dtcenter/common/loader/rdbms/db2/Db2ClientTest.java
@@ -3,21 +3,50 @@ package com.dtstack.dtcenter.common.loader.rdbms.db2;
 import com.dtstack.dtcenter.common.exception.DtCenterDefException;
 import com.dtstack.dtcenter.common.loader.rdbms.common.AbsRdbmsClient;
 import com.dtstack.dtcenter.loader.dto.SourceDTO;
+import com.dtstack.dtcenter.loader.dto.SqlQueryDTO;
 import org.junit.Test;
+
+import java.util.List;
 
 public class Db2ClientTest {
     private static AbsRdbmsClient rdbsClient = new Db2Client();
+    SourceDTO source = SourceDTO.builder()
+            .url("jdbc:db2://kudu5:50000/xiaochen")
+            .username("xiaochen")
+            .password("Abc1234")
+            .build();
+
+        SqlQueryDTO queryDTO = SqlQueryDTO.builder().tableName("EMPLOYEE").build();
+//    SqlQueryDTO queryDTO = SqlQueryDTO.builder().build();
+
+
 
     @Test
     public void getConnFactory() throws Exception {
-        SourceDTO source = SourceDTO.builder()
-                .url("jdbc:db2://kudu4:50000/dtstack")
-                .username("db2inst1")
-                .password("abc123")
-                .build();
         Boolean isConnected = rdbsClient.testCon(source);
         if (!isConnected) {
             throw new DtCenterDefException("数据源连接异常");
         }
+    }
+
+    @Test
+    public void getTableList() throws Exception {
+        List tableList = rdbsClient.getTableList(source, queryDTO);
+        System.out.println(tableList);
+
+    }
+
+    @Test
+    public void getTableMetaComment() throws Exception {
+
+        String tableMetaComment = rdbsClient.getTableMetaComment(source, queryDTO);
+        System.out.println(tableMetaComment);
+    }
+
+    @Test
+    public void getColumnMetaData() throws Exception {
+        List columnMetaData = rdbsClient.getColumnMetaData(source, queryDTO);
+        System.out.println(columnMetaData);
+
     }
 }
