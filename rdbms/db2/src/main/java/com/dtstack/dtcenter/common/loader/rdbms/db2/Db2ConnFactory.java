9365e4630bac5a874b09e8788312a84b75e9228f
晓辰 <xiaochen@dtstack.com>
2020-04-18 17:54:56 +0800

    Revert "fix db2"
    This reverts commit 098eb37d

--- rdbms/db2/src/main/java/com/dtstack/dtcenter/common/loader/rdbms/db2/Db2ConnFactory.java
+++ rdbms/db2/src/main/java/com/dtstack/dtcenter/common/loader/rdbms/db2/Db2ConnFactory.java
@@ -2,12 +2,6 @@ package com.dtstack.dtcenter.common.loader.rdbms.db2;
 
 import com.dtstack.dtcenter.common.enums.DataBaseType;
 import com.dtstack.dtcenter.common.loader.rdbms.common.ConnFactory;
-import com.dtstack.dtcenter.loader.dto.SourceDTO;
-import com.dtstack.dtcenter.loader.utils.DBUtil;
-import lombok.extern.slf4j.Slf4j;
-
-import java.sql.Connection;
-import java.sql.Statement;
 
 /**
  * @company: www.dtstack.com
@@ -15,26 +9,8 @@ import java.sql.Statement;
  * @Date ：Created in 16:09 2020/1/7
  * @Description：Db2 工厂类
  */
-@Slf4j
 public class Db2ConnFactory extends ConnFactory {
     public Db2ConnFactory() {
         this.driverName = DataBaseType.DB2.getDriverClassName();
-    }
-
-
-    @Override
-    public Boolean testConn(SourceDTO source) {
-        boolean isConnected = false;
-        Connection conn = null;
-        Statement statement = null;
-        try {
-            conn = getConn(source);
-            isConnected = true;
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        } finally {
-            DBUtil.closeDBResources(null, statement, conn);
-        }
-        return isConnected;
     }
 }
