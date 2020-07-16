7f46a8781d87abcc141103bdf666ae4ce10cc441
nanqi <nanqi@dtstack.com>
2020-07-16 16:59:54 +0800

    add Phonenix5 & hbase2

---
+++ core/src/main/java/com/dtstack/dtcenter/loader/dto/source/Phoenix5SourceDTO.java
@@ -0,0 +1,17 @@
+package com.dtstack.dtcenter.loader.dto.source;
+
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @company: www.dtstack.com
+ * @Author ：Nanqi
+ * @Date ：Created in 16:56 2020/7/16
+ * @Description：Phoenix5 数据源信息
+ */
+@Data
+@ToString
+@SuperBuilder
+public class Phoenix5SourceDTO extends RdbmsSourceDTO {
+}
