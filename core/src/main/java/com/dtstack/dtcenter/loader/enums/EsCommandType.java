e317561e0ff0ca785846011fd15cda2f8beefab9
jingxing <jingxing@dtstack.com>
2020-09-26 15:39:34 +0800

    add enum EsCommandType for ES

---
+++ core/src/main/java/com/dtstack/dtcenter/loader/enums/EsCommandType.java
@@ -0,0 +1,14 @@
+package com.dtstack.dtcenter.loader.enums;
+
+/**
+ *
+ * @Description: Elasticsearch 操作
+ * @Author: 景行
+ * @Date: 2020/9/18
+ */
+public enum EsCommandType {
+    INSERT,
+    UPDATE,
+    DELETE,
+    BULK;
+}
