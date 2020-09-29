f77674f951d2009c0bc734069c061933e284a97b
nanqi <nanqi@dtstack.com>
2020-09-29 22:34:40 +0800

    fix fastjson oracle serializer

--- core/src/main/java/com/dtstack/dtcenter/loader/client/sql/DataSourceClientFactory.java
+++ core/src/main/java/com/dtstack/dtcenter/loader/client/sql/DataSourceClientFactory.java
@@ -1,9 +1,13 @@
 package com.dtstack.dtcenter.loader.client.sql;
 
+import com.alibaba.fastjson.serializer.DateCodec;
+import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.dtstack.dtcenter.loader.ClassLoaderCallBack;
 import com.dtstack.dtcenter.loader.ClassLoaderCallBackMethod;
 import com.dtstack.dtcenter.loader.client.IClient;
+import com.dtstack.dtcenter.loader.source.DataSourceType;
 import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Iterator;
 import java.util.Map;
@@ -15,6 +19,7 @@ import java.util.ServiceLoader;
  * @Date ：Created in 15:50 2020/1/6
  * @Description：关系型数据库客户端工厂
  */
+@Slf4j
 public class DataSourceClientFactory {
     /**
      * 存储 插件名称 - ClassLoader 键值对信息
@@ -49,10 +54,28 @@ public class DataSourceClientFactory {
             return;
         }
 
+        dealFastJSON(pluginName, classLoader);
         pluginClassLoader.put(pluginName, classLoader);
     }
 
     public static boolean checkContainClassLoader(String pluginName) {
         return pluginClassLoader.containsKey(pluginName);
+    }
+
+    /**
+     * 特殊处理序列化逻辑
+     */
+    private static void dealFastJSON(String pluginName, ClassLoader classLoader) {
+        // 处理 oracle 时间类型字段
+        if (DataSourceType.Oracle.getPluginName().equals(pluginName)) {
+            try {
+                Class<?> loadClass = classLoader.loadClass("oracle.sql.DATE");
+                SerializeConfig.getGlobalInstance().put(loadClass, DateCodec.instance);
+                loadClass = classLoader.loadClass("oracle.sql.TIMESTAMP");
+                SerializeConfig.getGlobalInstance().put(loadClass, DateCodec.instance);
+            } catch (ClassNotFoundException e) {
+                log.error("FastJSON 序列化工具异常", e);
+            }
+        }
     }
 }
