15a54e381b75d65f21a4e8eae3fbb0658060af60
wangchuan <wangchuan@dtstack.com>
2020-10-26 16:02:31 +0800

    [feat][修改关系型数据库自定义查询使用字段使用别名，查询结果显示字段别名而不是真实字段名][]

--- common/src/main/java/com/dtstack/dtcenter/common/loader/common/utils/DBUtil.java
+++ common/src/main/java/com/dtstack/dtcenter/common/loader/common/utils/DBUtil.java
@@ -41,7 +41,7 @@ public class DBUtil {
             int columns = res.getMetaData().getColumnCount();
             List<String> columnName = Lists.newArrayList();
             for (int i = 0; i < columns; i++) {
-                columnName.add(res.getMetaData().getColumnName(i + 1));
+                columnName.add(res.getMetaData().getColumnLabel(i + 1));
             }
 
             while (res.next()) {
@@ -87,7 +87,7 @@ public class DBUtil {
             int columns = res.getMetaData().getColumnCount();
             List<String> columnName = Lists.newArrayList();
             for (int i = 0; i < columns; i++) {
-                columnName.add(res.getMetaData().getColumnName(i + 1));
+                columnName.add(res.getMetaData().getColumnLabel(i + 1));
             }
 
             while (res.next()) {
