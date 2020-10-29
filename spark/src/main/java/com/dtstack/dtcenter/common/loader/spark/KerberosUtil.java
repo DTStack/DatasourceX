fee3220b63be18a133b05e90318c30614cb690b9
nanqi <nanqi@dtstack.com>
2020-10-29 14:28:48 +0800

    fix kerberos log & fix hive yarn.resourcemanager.principal

--- spark/src/main/java/com/dtstack/dtcenter/common/loader/spark/KerberosUtil.java
+++ spark/src/main/java/com/dtstack/dtcenter/common/loader/spark/KerberosUtil.java
@@ -1,12 +1,11 @@
 package com.dtstack.dtcenter.common.loader.spark;
 
-import com.dtstack.dtcenter.common.exception.DtCenterDefException;
 import com.dtstack.dtcenter.common.hadoop.DtKerberosUtils;
-import com.dtstack.dtcenter.common.kerberos.KerberosConfigVerify;
-import com.google.common.base.Preconditions;
+import com.dtstack.dtcenter.common.hadoop.HadoopConfTool;
+import com.dtstack.dtcenter.loader.exception.DtLoaderException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.MapUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.security.UserGroupInformation;
 import sun.security.krb5.Config;
@@ -26,38 +25,40 @@ public class KerberosUtil {
     }
 
     public static synchronized UserGroupInformation loginKerberosWithUGI(Map<String, Object> confMap, String principal, String keytab, String krb5Conf) {
-        confMap = KerberosConfigVerify.replaceHost(confMap);
         principal = MapUtils.getString(confMap, principal);
         keytab = MapUtils.getString(confMap, keytab);
         krb5Conf = MapUtils.getString(confMap, krb5Conf);
+        // 兼容历史逻辑
         if (StringUtils.isNotEmpty(keytab) && !keytab.contains("/")) {
             keytab = MapUtils.getString(confMap, "keytabPath");
         }
-
-        Preconditions.checkState(StringUtils.isNotEmpty(keytab), "keytab can not be empty");
-        log.info("login kerberos, principal={}, path={}, krb5Conf={}", new Object[]{principal, keytab, krb5Conf});
-        Configuration config = DtKerberosUtils.getConfig(confMap);
+        // 如果前端没传 Principal 则直接从 Keytab 中获取第一个 Principal
         if (StringUtils.isEmpty(principal) && StringUtils.isNotEmpty(keytab)) {
             principal = DtKerberosUtils.getPrincipal(keytab);
         }
-
-        if (MapUtils.isEmpty(confMap) || StringUtils.isEmpty(principal) || StringUtils.isEmpty(keytab)) {
-            throw new DtCenterDefException("Kerberos Login fail, confMap or principal or keytab is null");
+        // 校验 Principal 和 Keytab 文件
+        if (StringUtils.isEmpty(principal) || StringUtils.isEmpty(keytab)) {
+            throw new DtLoaderException("Kerberos Login fail, principal or keytab is null");
         }
 
         try {
-            Config.refresh();
+            // 设置 Krb5 配置文件
             if (StringUtils.isNotEmpty(krb5Conf)) {
-                System.setProperty("java.security.krb5.conf", krb5Conf);
+                System.setProperty(HadoopConfTool.KEY_JAVA_SECURITY_KRB5_CONF, krb5Conf);
             }
 
+            // 开始 Kerberos 认证
+            log.info("login kerberos, currentUser={}, principal={}, principalFilePath={}, krb5ConfPath={}", UserGroupInformation.getCurrentUser(), principal, keytab, krb5Conf);
+            Config.refresh();
+            Configuration config = DtKerberosUtils.getConfig(confMap);
             config.set("hadoop.security.authentication", "Kerberos");
             UserGroupInformation.setConfiguration(config);
-            log.info("login kerberos, currentUser={}", new Object[]{UserGroupInformation.getCurrentUser(), principal, keytab, krb5Conf});
-            return UserGroupInformation.loginUserFromKeytabAndReturnUGI(principal, keytab);
+            UserGroupInformation ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI(principal, keytab);
+            log.info("login kerberos success, currentUser={}", UserGroupInformation.getCurrentUser());
+            return ugi;
         } catch (Exception var6) {
-            log.error("Login fail with config:{} \n {}", confMap, var6);
-            throw new DtCenterDefException("Kerberos Login fail", var6);
+            log.error("login kerberos failed, config:{}", confMap, var6);
+            throw new DtLoaderException("login kerberos failed", var6);
         }
     }
 }
