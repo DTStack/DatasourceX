e11b809beaded87a6b48693e4b2deb8f3bc5d81c
wangchuan <wangchuan@dtstack.com>
2020-10-29 12:50:01 +0800

    [fix][运行spark 的select 语句报错下载器reachedEnd失败][31801]

--- spark/src/main/java/com/dtstack/dtcenter/common/loader/spark/SparkParquetDownload.java
+++ spark/src/main/java/com/dtstack/dtcenter/common/loader/spark/SparkParquetDownload.java
@@ -120,19 +120,8 @@ public class SparkParquetDownload implements IDownloader {
         }
 
         ParquetReader.Builder<Group> reader = ParquetReader.builder(readSupport, new Path(currFile)).withConf(conf);
-        if (MapUtils.isNotEmpty(kerberosConfig)) {
-            build = KerberosUtil.loginKerberosWithUGI(kerberosConfig).doAs(
-                    (PrivilegedAction<ParquetReader<Group>>) () -> {
-                        try {
-                            return reader.build();
-                        } catch (IOException e) {
-                            throw new DtLoaderException(e.getMessage(), e);
-                        }
-                    }
-            );
-        } else {
-            build = reader.build();
-        }
+
+        build = reader.build();
 
         if(CollectionUtils.isNotEmpty(partitionColumns)){
             currentPartData = HdfsOperator.parsePartitionDataFromUrl(currFile, partitionColumns);
@@ -171,6 +160,23 @@ public class SparkParquetDownload implements IDownloader {
 
     @Override
     public List<String> readNext() throws Exception {
+
+        // 无kerberos认证
+        if (MapUtils.isEmpty(kerberosConfig)) {
+            return readNextWithKerberos();
+        }
+        // kerberos认证
+        return KerberosUtil.loginKerberosWithUGI(kerberosConfig).doAs(
+                (PrivilegedAction<List<String>>) ()->{
+                    try {
+                        return readNextWithKerberos();
+                    } catch (Exception e){
+                        throw new DtLoaderException("读取文件异常", e);
+                    }
+                });
+    }
+
+    public List<String> readNextWithKerberos() throws Exception {
         readNum++;
 
         List<String> line = null;
@@ -273,7 +279,20 @@ public class SparkParquetDownload implements IDownloader {
 
     @Override
     public boolean reachedEnd() throws Exception {
-        return !nextRecord();
+        // 无kerberos认证
+        if (MapUtils.isEmpty(kerberosConfig)) {
+            return !nextRecord();
+        }
+
+        // kerberos认证
+        return KerberosUtil.loginKerberosWithUGI(kerberosConfig).doAs(
+                (PrivilegedAction<Boolean>) ()->{
+                    try {
+                        return !nextRecord();
+                    } catch (Exception e){
+                        throw new DtLoaderException("下载文件异常", e);
+                    }
+                });
     }
 
     @Override
