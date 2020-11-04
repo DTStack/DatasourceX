bc357cd26c44ccb7279bde8165e67d2d1febb34b
zaiyue yu <ylivs@gmail.com>
2020-11-04 17:39:42 +0800

    增加hbase配置项可配置

--- hbase_gateway/src/main/java/com/dtstack/dtcenter/common/loader/hbase/pool/HbasePoolManager.java
+++ hbase_gateway/src/main/java/com/dtstack/dtcenter/common/loader/hbase/pool/HbasePoolManager.java
@@ -149,7 +149,7 @@ public class HbasePoolManager {
         hbaseMap.put("hbase.client.pause", "100");
         hbaseMap.put("zookeeper.recovery.retry", "3");
         hbaseMap.put("hbase.client.ipc.pool.type","RoundRobinPool");
-        hbaseMap.put("hbase.client.ipc.pool.size","10");
+        hbaseMap.put("hbase.client.ipc.pool.size",String.valueOf(hbaseSourceDTO.getPoolConfig().getMaximumPoolSize()));
 
         // 设置其他信息
         hbaseMap.putAll(JSONUtil.parseMap(hbaseSourceDTO.getOthers()));
