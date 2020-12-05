e60016b679c4bd5bbd6e28cf82c60dfee96a12ec
nanqi <nanqi@dtstack.com>
2020-12-05 17:28:20 +0800

    fix simple auth performance

--- core/src/test/java/com/dtstack/dtcenter/loader/client/sql/HiveTest.java
+++ core/src/test/java/com/dtstack/dtcenter/loader/client/sql/HiveTest.java
@@ -1,6 +1,7 @@
 package com.dtstack.dtcenter.loader.client.sql;
 
 import com.dtstack.dtcenter.common.exception.DtCenterDefException;
+import com.dtstack.dtcenter.common.thread.RdosThreadFactory;
 import com.dtstack.dtcenter.loader.IDownloader;
 import com.dtstack.dtcenter.loader.client.AbsClientCache;
 import com.dtstack.dtcenter.loader.client.IClient;
@@ -17,6 +18,8 @@ import java.sql.Connection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @company: www.dtstack.com
@@ -28,14 +31,15 @@ public class HiveTest {
     private static final AbsClientCache clientCache = ClientType.DATA_SOURCE_CLIENT.getClientCache();
 
     HiveSourceDTO source = HiveSourceDTO.builder()
-            .url("jdbc:hive2://172.16.100.219:10000/yeluo_test")
-            .schema("yeluo_test")
+            .url("jdbc:hive2://172.16.8.107:10000")
+            .schema("default")
+            .username("admin")
             .defaultFS("hdfs://ns1")
             .config("{\n" +
                     "    \"dfs.ha.namenodes.ns1\": \"nn1,nn2\",\n" +
-                    "    \"dfs.namenode.rpc-address.ns1.nn2\": \"172.16.100.219:9000\",\n" +
+                    "    \"dfs.namenode.rpc-address.ns1.nn2\": \"kudu2:9000\",\n" +
                     "    \"dfs.client.failover.proxy.provider.ns1\": \"org.apache.hadoop.hdfs.server.namenode.ha.ConfiguredFailoverProxyProvider\",\n" +
-                    "    \"dfs.namenode.rpc-address.ns1.nn1\": \"172.16.100.204:9000\",\n" +
+                    "    \"dfs.namenode.rpc-address.ns1.nn1\": \"kudu1:9000\",\n" +
                     "    \"dfs.nameservices\": \"ns1\"\n" +
                     "}")
             .build();
@@ -77,6 +81,28 @@ public class HiveTest {
             IClient client = clientCache.getClient(DataSourceType.HIVE.getPluginName());
             SqlQueryDTO queryDTO = SqlQueryDTO.builder().sql("select * from nanqi0000000").build();
             List<Map<String, Object>> mapList = client.executeQuery(source, queryDTO);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void executeQueryForThreadTest() throws Exception {
+        try {
+            IClient client = clientCache.getClient(DataSourceType.HIVE.getPluginName());
+            SqlQueryDTO queryDTO = SqlQueryDTO.builder().sql("select * from nanqi1030").build();
+            List<Map<String, Object>> mapList = client.executeQuery(source, queryDTO);
+            ExecutorService threadPool = Executors.newFixedThreadPool(6, new RdosThreadFactory("test_nanqi"));
+            for (int i = 0; i < 20000000; i++) {
+                threadPool.submit(() -> {
+                    try {
+                        client.executeQuery(source, queryDTO);
+                    } catch (Exception e) {
+
+                    }
+                });
+            }
+            Thread.sleep(1000000L);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
