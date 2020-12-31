0409d8cafa94824fad2ba2acc3bef4ca75edfbcd
wangchuan <wangchuan@dtstack.com>
2020-12-31 17:17:02 +0800

    [feat][hbase数据预览返回条数不对bug修复][33386]

--- core/src/test/java/com/dtstack/dtcenter/loader/client/sql/Hbase2Test.java
+++ core/src/test/java/com/dtstack/dtcenter/loader/client/sql/Hbase2Test.java
@@ -159,7 +159,7 @@ public class Hbase2Test {
     @Test
     public void preview() throws Exception {
         IClient client = ClientCache.getClient(DataSourceType.HBASE2.getVal());
-        List<List<Object>> result = client.getPreview(source, SqlQueryDTO.builder().tableName("wangchuan_test").previewNum(2).build());
+        List<List<Object>> result = client.getPreview(source, SqlQueryDTO.builder().tableName("log1").previewNum(2).build());
         System.out.println(result);
     }
 }
