165555175ad1f6955208dad4958b763645d39ece
qianyi <>
2021-04-22 10:32:05 +0800

    [feat_4.2.x_issue#114][common-loader-oceanbase][支持oceanbase]

--- core/src/main/java/com/dtstack/dtcenter/loader/source/DataBaseType.java
+++ core/src/main/java/com/dtstack/dtcenter/loader/source/DataBaseType.java
@@ -38,7 +38,7 @@ public enum DataBaseType {
     Phoenix5("Phoenix5", "org.apache.phoenix.jdbc.PhoenixDriver"),
     KINGBASE8("kingbase8", "com.kingbase8.Driver"),
     Presto("presto", "com.facebook.presto.jdbc.PrestoDriver", "select 1111"),
-    OceanBase("oceanbase","com.alipay.oceanbase.jdbc.Driver","show tables");
+    OceanBase("oceanbase","com.alipay.oceanbase.jdbc.Driver","select 1111");
 
     private String typeName;
     private String driverClassName;
