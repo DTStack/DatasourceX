27451cd2e61fedc6c528aee7db725a90ca3725cc
wangchuan <wangchuan@dtstack.com>
2021-06-18 16:02:55 +0800

    [feat_4.2.x_influxDB_preview][common-loader][influxDB 数据预览修复]

--- influxdb/src/main/java/com/dtstack/dtcenter/common/loader/influxdb/InfluxDBClient.java
+++ influxdb/src/main/java/com/dtstack/dtcenter/common/loader/influxdb/InfluxDBClient.java
@@ -11,9 +11,11 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.influxdb.InfluxDB;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * influxDB 客户端
@@ -31,7 +33,7 @@ public class InfluxDBClient<T> extends AbsNoSqlClient<T> {
     private static final String SHOW_TABLE = "SHOW measurements";
 
     // 数据预览并限制条数 SQL
-    private static final String PREVIEW_LIMIT = "SELECT * FROM %s LIMIT %s";
+    private static final String PREVIEW_LIMIT = "SELECT %s FROM %s LIMIT %s";
 
     // 获取表字段信息 SQL
     private static final String SHOW_FIELD = "SHOW field keys from %s";
@@ -66,7 +68,12 @@ public class InfluxDBClient<T> extends AbsNoSqlClient<T> {
         if (StringUtils.isBlank(queryDTO.getTableName())) {
             throw new DtLoaderException("table name cannot be empty.");
         }
-        return InfluxDBUtil.queryWithList(influxDB, String.format(PREVIEW_LIMIT, queryDTO.getTableName(), queryDTO.getPreviewNum()), true);
+        List<ColumnMetaDTO> columnMetaData = getColumnMetaData(source, queryDTO);
+        if (CollectionUtils.isEmpty(columnMetaData)) {
+            return Collections.emptyList();
+        }
+        List<String> fieldList = columnMetaData.stream().map(col -> String.format("\"%s\"", col.getKey())).collect(Collectors.toList());
+        return InfluxDBUtil.queryWithList(influxDB, String.format(PREVIEW_LIMIT, String.join(",", fieldList), queryDTO.getTableName(), queryDTO.getPreviewNum()), true);
     }
 
     @Override
