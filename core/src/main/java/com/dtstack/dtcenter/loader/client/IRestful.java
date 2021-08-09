614f012257e7ee130af0f825f9063fa1e0b2e9ce
wangchuan <wangchuan@dtstack.com>
2021-08-09 14:48:37 +0800

    [feat_4.3.x_restful][common-loader][支持 restful 数据源]

---
+++ core/src/main/java/com/dtstack/dtcenter/loader/client/IRestful.java
@@ -0,0 +1,12 @@
+package com.dtstack.dtcenter.loader.client;
+
+/**
+ * <p>提供 Restful 相关操作方法</p>
+ *
+ * @author ：wangchuan
+ * date：Created in 上午10:06 2021/8/9
+ * company: www.dtstack.com
+ */
+public interface IRestful {
+
+}
