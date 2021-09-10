0725db8052541dd9fee0c591a95b7efbe8aa7600
wangchuan <wangchuan@dtstack.com>
2021-09-10 15:28:03 +0800

    [hotfix_4.1.x_43454][common-loader][hive 、spark 数据预览 sql 加 limit]

--- hive3/src/main/java/com/dtstack/dtcenter/common/loader/hive3/client/Hive3Client.java
+++ hive3/src/main/java/com/dtstack/dtcenter/common/loader/hive3/client/Hive3Client.java
@@ -447,7 +447,7 @@ public class Hive3Client extends AbsRdbmsClient {
             }
         }
 
-        return "select * from " + sqlQueryDTO.getTableName() + partSql.toString();
+        return "select * from " + sqlQueryDTO.getTableName() + partSql.toString() + limitSql(sqlQueryDTO.getPreviewNum());
     }
 
     @Override
