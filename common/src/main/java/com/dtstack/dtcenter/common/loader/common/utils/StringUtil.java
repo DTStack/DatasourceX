404ea3e17924963505bb38a0339906a417b3b24d
qianyi <>
2021-09-16 17:55:57 +0800

    [hotifix_4.3.x_43647][common.loader.hive common.loader.spark][select * 预览数据数据乱码]

--- common/src/main/java/com/dtstack/dtcenter/common/loader/common/utils/StringUtil.java
+++ common/src/main/java/com/dtstack/dtcenter/common/loader/common/utils/StringUtil.java
@@ -3,11 +3,11 @@ package com.dtstack.dtcenter.common.loader.common.utils;
 public class StringUtil {
     private static final char[] DIGITS_LOWER = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
-    public static char[] encodeHex(byte[] data) {
+    public static String encodeHex(byte[] data) {
         return encodeHex(data, DIGITS_LOWER);
     }
 
-    public static char[] encodeHex(byte[] data, char[] toDigits) {
+    public static String encodeHex(byte[] data, char[] toDigits) {
         int length = data.length;
         char[] out = new char[length * 3];
         int i = 0;
@@ -17,6 +17,6 @@ public class StringUtil {
             out[var++] = ' ';
         }
 
-        return out;
+        return String.valueOf(out);
     }
 }
